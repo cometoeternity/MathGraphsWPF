a2bf8c2495473242aacf25551086d73a959a8f68
dotoeternity@outlook.com <dotoeternity@outlook.com>
2020-07-08 21:30:34 +0700

    Сделана основная страница приложения.

--- MathGraphView/WpfGraphView/MainWindow.xaml.cs
+++ MathGraphView/WpfGraphView/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 using OxyPlot;
 using OxyPlot.Series;
 
@@ -26,6 +27,92 @@ namespace WpfGraphView
         {
             InitializeComponent();
         }
+        private void VisibilityAndIsOpen()
+        {
+            popup_uc.Visibility = Visibility.Collapsed;
+            popup_uc.IsOpen = false;
+        }
+        private void sin_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = sin;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Синус";
+        }
 
+        private void sin_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisibilityAndIsOpen();
+        }
+
+        private void cos_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = cos;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Косинус";
+        }
+
+        private void cos_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisibilityAndIsOpen();
+        }
+
+        private void tg_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = tg;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Тангенс";
+        }
+
+        private void tg_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisibilityAndIsOpen();
+        }
+
+        private void ctg_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = ctg;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Катангенс";
+        }
+
+        private void ctg_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisibilityAndIsOpen();
+        }
+
+        private void exp_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = exp;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Экспанента";
+        }
+
+        private void exp_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisibilityAndIsOpen();
+        }
+
+        private void log_MouseEnter(object sender, MouseEventArgs e)
+        {
+            popup_uc.PlacementTarget = log;
+            popup_uc.Placement = PlacementMode.Right;
+            popup_uc.IsOpen = true;
+            Header.PopupText.Text = "Логарифм";
+        }
+
+        private void log_MouseLeave(object sender, MouseEventArgs e)
+        {
+            VisibilityAndIsOpen();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
